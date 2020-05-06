7ec443b715b4d979b9c92a45e8c7facf0f599b2d
Nolan Trapp <officialstackemup@gmail.com>
2020-05-06 15:57:51 -0700

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.login(process.env.token);
+
+bot.on('ready', () =>{
+    console.log('Boomer&Dinky are Online!');
+})
+
+
+bot.on('message', msg=>{
+    if(msg.content === 'Ok Boomer'){
+
+        msg.reply('Hey, your the boomer, not me!');
+    }
+    })
